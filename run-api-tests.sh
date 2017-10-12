a8d6d9a36d34d48eca5098e617343d2c63e1786c
Anish Karandikar <anishkny@gmail.com>
2017-10-12 14:54:37 -0400

    chore(test): Use actual GCP_PROJECT for functions emulator

--- run-api-tests.sh
+++ run-api-tests.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash -e -x
 pwd
 
 ## Deploy to Cloud Functions Local Emulator
 export DATASTORE_NAMESPACE=test-api
 which functions
-echo mock-project | functions start
+echo $GCP_PROJECT | functions start
 if [ -n "$CI" ]; then
   functions config set watch false
   functions stop
@@ -16,8 +16,9 @@ functions deploy api --trigger-http | tee $DEPLOY_OUTPUT_FILE
 export API_URL=`grep Resource $DEPLOY_OUTPUT_FILE | grep -o 'http://localhost:[^[:space:]]*'`
 echo $API_URL
 
+## Smoke test API endpoint
+curl $API_URL/ping
+
 ## Run Postman tests against local deployed API
 which newman
 newman run --global-var="apiUrl=$API_URL" --folder automated ./api-tests.postman.json
-
-functions stop
