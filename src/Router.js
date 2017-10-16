88cb9c58de23ea843414e650954b72377e0eb0f3
Anish Karandikar <anishkny@gmail.com>
2017-10-15 22:58:23 -0400

    refactor(Router): Loop over array of routes instead of if-else chain

--- src/Router.js
+++ src/Router.js
@@ -18,55 +18,79 @@ module.exports = {
       }
     }
 
-    if (req.path == '/ping') {
-      res.status(200).send({
+    // Define routes that can be handled
+    const routes = [
+
+      // Helpers
+      ['GET', '/ping', async() => res.status(200).send({
         pong: new Date(),
         DATASTORE_NAMESPACE: process.env.DATASTORE_NAMESPACE ? process.env.DATASTORE_NAMESPACE : '',
-      });
-    } else if (req.method == 'POST' && req.path == '/users') {
-      res.status(200).send({ user: await User.create(req.body.user) });
-    } else if (req.method == 'POST' && req.path == '/users/login') {
-      res.status(200).send({ user: await User.login(req.body.user) });
-    } else if (req.method == 'GET' && req.path.startsWith('/profiles/')) {
-      const matchedPath = (new routeParser('/profiles/:username')).match(req.path);
-      res.status(200).send({ profile: await User.getProfile(matchedPath.username, validatedUser) });
-    } else if (req.method == 'POST' && req.path.startsWith('/profiles/')) {
-      const matchedPath = (new routeParser('/profiles/:username')).match(req.path);
-      res.status(200).send({ profile: await User.followUser(validatedUser.username, matchedPath.username) });
-    } else if (req.method == 'DELETE' && req.path.startsWith('/profiles/')) {
-      const matchedPath = (new routeParser('/profiles/:username')).match(req.path);
-      res.status(200).send({ profile: await User.unfollowUser(validatedUser.username, matchedPath.username) });
-    } else if (req.method == 'GET' && req.path == '/user') {
-      if (!validatedUser) {
-        res.status(401).send({ errors: { body: ['Token is required'], }, });
+      })],
+
+      // Users
+      ['POST', '/users/login', async() => res.status(200).send({ user: await User.login(req.body.user) })],
+      ['POST', '/users', async() => res.status(200).send({ user: await User.create(req.body.user) })],
+      ['GET', '/user', async() => {
+        if (!validatedUser) {
+          res.status(401).send({ errors: { body: ['Token is required'], }, });
+        }
+        res.status(200).send({ user: validatedUser });
+      }],
+
+      // Profiles
+      ['GET', '/profiles/:username', async(matchedPath) => res.status(200).send({
+        profile: await User.getProfile(matchedPath.username, validatedUser)
+      })],
+      ['POST', '/profiles/:username/follow', async(matchedPath) => res.status(200).send({
+        profile: await User.followUser(validatedUser.username, matchedPath.username)
+      })],
+      ['DELETE', '/profiles/:username/follow', async(matchedPath) => res.status(200).send({
+        profile: await User.unfollowUser(validatedUser.username, matchedPath.username)
+      })],
+
+      // Articles
+      ['GET', '/articles', async() => {
+        const articles = await Article.getAll({
+          tag: req.query.tag,
+          author: req.query.author,
+          limit: req.query.limit,
+          offset: req.query.offset,
+          reader: validatedUsername,
+        });
+        res.status(200).send({ articles, articlesCount: articles.length });
+      }],
+      ['POST', '/articles', async() => {
+        if (!validatedUser) {
+          res.status(401).send({ errors: { body: ['Must be logged in'], }, });
+        }
+        res.status(200).send({ article: await Article.create(req.body.article, validatedUsername) });
+      }],
+      ['GET', '/articles/:slug', async(matchedPath) => res.status(200).send({
+        article: await Article.get(matchedPath.slug, validatedUsername)
+      })],
+      ['DELETE', '/articles/:slug', async(matchedPath) => {
+        if (!validatedUser) {
+          res.status(401).send({ errors: { body: ['Must be logged in'], }, });
+        }
+        res.status(200).send(await Article.delete(matchedPath.slug, validatedUsername));
+      }],
+
+    ];
+
+    // Match route and call handler
+    for (let i = 0; i < routes.length; ++i) {
+      if (req.method !== routes[i][0]) {
+        continue;
       }
-      res.status(200).send({ user: validatedUser });
-    } else if (req.method == 'GET' && req.path == '/articles') {
-      const articles = await Article.getAll({
-        tag: req.query.tag,
-        author: req.query.author,
-        limit: req.query.limit,
-        offset: req.query.offset,
-        reader: validatedUsername,
-      });
-      res.status(200).send({ articles, articlesCount: articles.length });
-    } else if (req.method == 'POST' && req.path == '/articles') {
-      if (!validatedUser) {
-        res.status(401).send({ errors: { body: ['Must be logged in'], }, });
+      const matchedPath = (new routeParser(routes[i][1])).match(req.path);
+      if (matchedPath) {
+        await routes[i][2](matchedPath);
+        return;
       }
-      res.status(200).send({ article: await Article.create(req.body.article, validatedUsername) });
-    } else if (req.method == 'GET' && req.path.startsWith('/articles/')) {
-      const matchedPath = (new routeParser('/articles/:slug')).match(req.path);
-      res.status(200).send({ article: await Article.get(matchedPath.slug, validatedUsername) });
-    } else if (req.method == 'DELETE' && req.path.startsWith('/articles/')) {
-      if (!validatedUser) {
-        res.status(401).send({ errors: { body: ['Must be logged in'], }, });
-      }
-      const matchedPath = (new routeParser('/articles/:slug')).match(req.path);
-      res.status(200).send(await Article.delete(matchedPath.slug, validatedUsername));
-    } else {
-      res.status(404).send({ errors: { body: [`404 Not found: [${req.method} ${req.path}]`], }, });
     }
+
+    // No routes were matched, respond with 404
+    res.status(404).send({ errors: { body: [`404 Not found: [${req.method} ${req.path}]`], }, });
 
   },
 
