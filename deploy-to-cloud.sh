41b6fd725c51e93ec8e79dfd2d2c029d1e2d2414
Anish Karandikar <anishkny@gmail.com>
2018-07-26 06:18:51 -0400

    feat(node): Use Node.js 8 runtime in GCF (#24)
    * feat(node): Use Node.js 8 runtime in GCF
    
    See: https://cloud.google.com/functions/docs/concepts/nodejs-8-runtime
    
    Fixes #23
    
    * chore(ci): Sleep longer after deploy and slower API tests

--- deploy-to-cloud.sh
+++ deploy-to-cloud.sh
@@ -1,22 +1,14 @@
-#!/bin/bash -e
-
-## Transpile code to Node 6 (needed for Cloud Functions)
-rm -rf dist/
-babel index.js src/*.js --presets es2015,stage-3 --out-dir dist/ --ignore *.test.js
-
-## Setup dist folder with additional files
-echo DATASTORE_NAMESPACE=test-api-cloud-`date +%s` > dist/.env
-cp package.json dist/
+#!/bin/bash -e -x
 
 ## Deploy to Cloud Functions
 GCLOUD_BIN="${GCLOUD_BIN:-gcloud}"
 echo GCLOUD_BIN=$GCLOUD_BIN
 echo Deploying function...
-$GCLOUD_BIN beta functions deploy api --source=dist/ --trigger-http
+$GCLOUD_BIN beta functions deploy api --runtime nodejs8 --set-env-vars DATASTORE_NAMESPACE=test-api-cloud-`date +%s` --trigger-http
 echo ...Done
 export API_URL=`$GCLOUD_BIN beta functions describe api --format=text | grep 'httpsTrigger.url:' | grep -o 'https://.*'`
 echo API_URL=$API_URL
-sleep 5
+sleep 60
 
 ## Smoke test API endpoint
 curl --silent $API_URL/ping
@@ -24,4 +16,4 @@ echo
 
 ## Run Postman tests against API deployed to cloud
 echo "Using newman runner located at: ["`which newman`"]"
-newman run ./api-tests.postman.json --global-var "apiUrl=$API_URL" --delay-request 500 --bail || true
+newman run ./api-tests.postman.json --global-var "apiUrl=$API_URL" --delay-request 1000 --bail || true
